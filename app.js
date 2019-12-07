75aa4c41b4e31eb80b5ae1f207272e4773f55fab
Andy Alvarado <AndyAlvarado1997@gmail.com>
2019-12-06 20:52:53 -0500

    init

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+
+app.get('/', function (req, res){
+    res.sendFile(__dirname + '/index.html');
+})
+
+app.use('/scripts', express.static(__dirname + '/node_modules/'));
+
+http.listen(3000, function(){
+    console.log('Listening on port 3000');
+})
